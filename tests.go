26d03e506f0e63662cadef3c937a9f9fec3fb537
Ersi Ni <ersi.ni@nilbot.net>
2016-02-24 13:26:51 +0000

    added some official test strings

--- tests.go
+++ tests.go
@@ -47,3 +47,18 @@ func getMemoryBenchmarkTests() []struct {
 	}
 	return rst
 }
+
+const input1 = `this little piggy`
+const input2 = `what has happened`
+const input3 = `in Private say one thing in Public say another`
+const input4 = `when speaking Privately to (12 or more) be discreet`
+const input5 = `there are "strings of things" and there are "chickens
+with
+wings"
+`
+const input6 = `over and over and over again`
+const input7 = `888 is quite ok but 88888 is "not ok"`
+const input8 = `PublicAndPrivateShouldNotBeConfused`
+const input9 = `when can ~ appear in a string? "Here An~d~ Th~~~~~~ere"
+but "some string
+`
